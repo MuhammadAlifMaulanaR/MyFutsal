aae8084593d44ffc9e04557473db489c80927466
MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>
2024-08-24 23:17:30 +0800

    Modify object lapanganService and modify case 1,2,3,4 LapanganView.java
    Signed-off-by: MuhammadAlifMaulanaR <muhalif.maulana17@gmail.com>

--- src/com/workshop/view/LapanganView.java
+++ src/com/workshop/view/LapanganView.java
@@ -4,17 +4,17 @@ import com.workshop.repository.JenisLapanganRepo;
 import com.workshop.repository.LapanganRepo;
 import com.workshop.model.JenisLapangan;
 import com.workshop.model.Lapangan;
+import com.workshop.service.LapanganService;
 
+import java.net.IDN;
 import java.util.List;
 import java.util.Scanner;
 
 public class LapanganView {
-    private LapanganRepo lapanganRepo;
-    private JenisLapanganRepo jenisLapanganRepo;
+    private LapanganService lapanganService;
 
-    public LapanganView(LapanganRepo lapanganRepo, JenisLapanganRepo jenisLapanganRepo){
-        this.lapanganRepo = lapanganRepo;
-        this.jenisLapanganRepo = jenisLapanganRepo;
+    public LapanganView(LapanganService lapanganService){
+        this.lapanganService = lapanganService;
     }
     public void ViewLapangans(Scanner scanner){
         while (true){
@@ -24,50 +24,44 @@ public class LapanganView {
             System.out.println("4. DELETE Lapangan");
             System.out.println("5. Back to Menu");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline
+            scanner.nextLine();
             switch (choice) {
                 case 1:
                     System.out.println("Enter name: ");
                     String name = scanner.nextLine();
                     System.out.println("Enter ID jenis lapangan: ");
-                    int ID = scanner.nextInt();
-                    JenisLapangan jenisLapangan = jenisLapanganRepo.select().stream().filter(j -> j.getID() == ID).findFirst().orElse(null);
-                    if (jenisLapangan != null) {
-                        lapanganRepo.create(new Lapangan(name, jenisLapangan));
-                    } else {
-                        System.out.println("Jenis lapangan not found.");
-                    }
+                    int jenisLapanganID = scanner.nextInt();
+                    scanner.nextLine();
+                    lapanganService.create(name, jenisLapanganID);
                     break;
                 case 2:
-                    List<Lapangan> lapangans = lapanganRepo.select();
+                    List<Lapangan> lapangans = lapanganService.getAllLapangans();
                     for (Lapangan lapangan : lapangans) {
                         System.out.println("ID: " + lapangan.getID() + ", Name: " + lapangan.getName() + ", Jenis Lapangan: " + lapangan.getJenisLapangan().getName());
                     }
                     break;
                 case 3:
-                    System.out.println("Enter ID: ");
-                    int IDlapangan = scanner.nextInt();
-                    scanner.nextLine(); // consume newline
+                    System.out.println("Enter ID lapangan: ");
+                    int ID = scanner.nextInt();
+                    scanner.nextLine();
                     System.out.println("Enter new name: ");
                     name = scanner.nextLine();
-                    System.out.println("Enter new jenis lapangan ID: ");
-                    ID = scanner.nextInt();
-                    jenisLapangan = jenisLapanganRepo.select().stream().filter(j -> j.getID() == ID).findFirst().orElse(null);
-                    if (jenisLapangan != null) {
-                        Lapangan lapangan = new Lapangan(name, jenisLapangan);
-                        lapangan.setID(IDlapangan);
-                        lapanganRepo.update(lapangan);
-                    } else {
-                        System.out.println("Jenis lapangan not found.");
-                    }
+                    System.out.println("Enter new ID jenis lapangan: ");
+                    jenisLapanganID = scanner.nextInt();
+                    scanner.nextLine();
+                    lapanganService.updateLapangan(ID,name,jenisLapanganID);
                     break;
                 case 4:
-                    System.out.println("Enter ID: ");
-                    IDlapangan = scanner.nextInt();
-                    lapanganRepo.delete(IDlapangan);
+                    System.out.println("Enter ID lapangan: ");
+                    ID = scanner.nextInt();
+                    scanner.nextLine();
+                    lapanganService.deleteLapangan(ID);
+                    System.out.println("Lapangan deleted successfully.");
                     break;
                 case 5:
                     return;
+                default:
+                    System.out.println("Invalid choice. Please choose again.");
             }
         }
     }
